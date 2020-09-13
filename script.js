47ea33adf7c76a46cf72e291aecf3ae6ec635684
FFirtes <65233296+FFirtes@users.noreply.github.com>
2020-09-13 22:41:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-alert ("Welcome to the new RPG game!")
-alert ("Prologue ...")
-alert ("This world is called Dexterian.\nThere are only 3 races in it: Humans, Elves, Demons.\nThese races are inhabited by: civilians, warriors, archers, wizards.")
+alert("Welcome to the new RPG game!")
+alert("Prologue ...")
+alert("This world is called Dexterian.\nThere are only 3 races in it: Humans, Elves, Demons.\nThese races are inhabited by: civilians, warriors, archers, wizards.")
+//info
+var h_race = confirm("Do you want to choose a race of Humans?")
+function race(){
+  if(h_race == true){
+      var h_class_warrior = confirm("Do you want to choose a class of Warrior?")
+          if(h_class_warrior == true){
+               alert("Your race is Humans and class is Warrior.")
+}}}
