b390355776cc9beb119450e87b5acc49758a7d5c
FFirtes <65233296+FFirtes@users.noreply.github.com>
2020-09-13 19:40:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 alert ("Welcome to the new RPG game!")
 alert ("Prologue ...")
-alert ("This world is called Dexterian.\nThere are only 3 races in it: Humans, Elves, Demons.")
+alert ("This world is called Dexterian.\nThere are only 3 races in it: Humans, Elves, Demons.\nThese races are inhabited by: civilians, warriors, archers, wizards.")
